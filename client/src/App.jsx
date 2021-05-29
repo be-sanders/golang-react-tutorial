c33ff5e06237c770ffc7770336c39a19c7464c4c
Brian Sanders <sanders.brian.eric@gmail.com>
2021-05-28 16:15:19 -1000

    starting to learn about jsx now

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import ReactDataSheet from 'react-datasheet';
+// Be sure to include styles at some point, probably during boostrapping
+import 'react-datasheet/lib/react-datasheet.css'
+
+class App extends React.Component {
+    constructor (props) {
+      super(props)
+      this.state = {
+        grid: [
+          [{value:  1}, {value:  3}],
+          [{value:  2}, {value:  4}]
+        ]
+      }
+    }
+    render () {
+      return (
+        <ReactDataSheet
+          data={this.state.grid}
+          valueRenderer={(cell) => cell.value}
+          onCellsChanged={changes => {
+            const grid = this.state.grid.map(row => [...row])
+            changes.forEach(({cell, row, col, value}) => {
+              grid[row][col] = {...grid[row][col], value}
+            })
+            this.setState({grid})
+          }}
+        />
+      )
+    }
+  }
